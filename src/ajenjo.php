b68653be028a2f84a17679dc3b5a850cef4bb1b0
Jonathan Delgado Zamorano <jonad.correo@gmail.com>
2014-10-10 20:44:37 -0300

    v0.5.4

--- src/ajenjo.php
+++ src/ajenjo.php
@@ -1,6 +1,8 @@
 <?php
 
-// namespace ajenjo;
+namespace ajenjo\laravel;
+
+// exit;
 
 class ajenjo {
 
@@ -12,22 +14,22 @@ class ajenjo {
 	static $created_at;
 
 	static function __load() {
-		$aje = new ajenjo\connect(
-			Config::get('ajenjo.base_uri', 'http://localhost/'),
+		$aje = new ajenjo\tools\connect(
+			Config::get('ajenjo.base_uri', 'http://localhost:8000/'),
 			Config::get('ajenjo.path_url', '/ajenjo/api/json')
 		);// Carga el objeto ajenjo conect
-		if (Session::has(Config::get('ajenjo.cookie', 'ajenjo_sesion'))) {// Verifica si existe un token en la sesion actual
+		if (Illuminate\Support\Facades\Session::has(Config::get('ajenjo.cookie', 'ajenjo_sesion'))) {// Verifica si existe un token en la sesion actual
 			//Asigna el token almacenada en la sesión dentro de la variable token
-			$aje->token = Session::get(Config::get('ajenjo.cookie', 'ajenjo_sesion'));
+			$aje->token = Illuminate\Support\Facades\Session::get(Config::get('ajenjo.cookie', 'ajenjo_sesion'));
 		} else {
 			// Genera un token nuevo
 			$aje->token = $aje->generateToken();
 			// Almacena el token en la variable de sesion
-			Session::put(Config::get('ajenjo.cookie', 'ajenjo_sesion'),$aje->token);
+			Illuminate\Support\Facades\Session::put(Config::get('ajenjo.cookie', 'ajenjo_sesion'),$aje->token);
 		}
 
-		if (Session::has('ajenjo_sesion$d')) {// verifica si existe almacenada una variable dentro de esta sesión
-			self::$d = Session::get('ajenjo_sesion$d');// Captura la memoria de la sesion y la carga a la variable $d
+		if (Illuminate\Support\Facades\Session::has('ajenjo_sesion$d')) {// verifica si existe almacenada una variable dentro de esta sesión
+			self::$d = Illuminate\Support\Facades\Session::get('ajenjo_sesion$d');// Captura la memoria de la sesion y la carga a la variable $d
 			self::$user = self::$d->user;// almacena al usuario
 			self::$auth = self::$d->auth;// almacena la memoria dentro la variable $d
 		} else {
@@ -42,7 +44,7 @@ class ajenjo {
 	static function check() {
 		self::__load();// carga a __load
 		$return = self::$d->check();// Captura check original
-		Session::put('ajenjo_sesion$d',self::$d);// carga la memoria $d a la memoria de sesion
+		Illuminate\Support\Facades\Session::put('ajenjo_sesion$d',self::$d);// carga la memoria $d a la memoria de sesion
 		return $return;
 	}
 
@@ -54,7 +56,7 @@ class ajenjo {
 		$return = self::$d->login($user, $password);// Carga login original
 		self::$user = self::$d->user;
 		self::$auth = self::$d->auth;
-		Session::put('ajenjo_sesion$d',self::$d);// carga la memoria $d a la memoria de sesion
+		Illuminate\Support\Facades\Session::put('ajenjo_sesion$d',self::$d);// carga la memoria $d a la memoria de sesion
 		return $return;
 	}
 
@@ -66,7 +68,7 @@ class ajenjo {
 		$return = self::$d->logout();// carga logout original
 		self::$user = self::$d->user;
 		self::$auth = self::$d->auth;
-		Session::put('ajenjo_sesion$d',self::$d);// carga la memoria $d a la memoria de sesion
+		Illuminate\Support\Facades\Session::put('ajenjo_sesion$d',self::$d);// carga la memoria $d a la memoria de sesion
 		self::destroy();
 		return $return;
 	}
@@ -84,8 +86,8 @@ class ajenjo {
 		$updated_at = null;
 		$created_at = null;
 		// Elimina las variables de sesion actual
-		Session::forget('ajenjo_sesion$d');
-		Session::forget(Config::get('ajenjo.cookie', 'ajenjo_sesion'));
+		Illuminate\Support\Facades\Session::forget('ajenjo_sesion$d');
+		Illuminate\Support\Facades\Session::forget(Config::get('ajenjo.cookie', 'ajenjo_sesion'));
 		return true;
 	}
 
